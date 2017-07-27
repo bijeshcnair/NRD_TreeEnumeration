45efaffdb10843b1842778685e4e79ee4e830525
root <root@1a09decc5662>
2017-07-27 13:02:27 +0000

    Initial Push

---
+++ src/main/webapp/wmProperties.js
@@ -0,0 +1,13 @@
+var _WM_APP_PROPERTIES = {
+  "activeTheme" : "mobile",
+  "dateFormat" : "",
+  "defaultLanguage" : "en",
+  "displayName" : "NRD-TreeEnumeration",
+  "homePage" : "Main",
+  "name" : "NRD_TreeEnumeration",
+  "platformType" : "MOBILE",
+  "supportedLanguages" : "en",
+  "timeFormat" : "",
+  "type" : "APPLICATION",
+  "version" : "1.0"
+};
